e98cb439d28e2cd084899f3615ad60cd049b25c7
Charlye Tran <tran1226@yahoo.com>
2016-02-02 18:57:07 -0500

    added functionality to add items to the grocery list

--- src/app/groceryList/groceryList.controller.js
+++ src/app/groceryList/groceryList.controller.js
@@ -17,8 +17,15 @@
       self.deleteItem = function(item) {
         var index = self.groceryList.indexOf(item);
         self.groceryList.splice(index, 1);
+      };
+      self.addItem = function(){
+        console.log('item to add to the grocery list is: ' + self.itemToAdd);
+        if (self.addItemToForm.$valid) {
+          self.groceryList.push(self.itemToAdd);
+          self.itemToAdd = "";
+          self.addItemToForm.$setPristine();
+        } 
       }
-
     });
 
 })();
